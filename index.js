1e4e0d2b8e6613cffc37c97aabb42458d6ccdac1
divyagovindaiah <divyag@sanketika.in>
2022-12-06 17:59:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express= require('express')
+const app= express()
+const fs= require('fs');
+
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost${port}`)
+});
+app.get('/read', function(req, res) {
+   const data =  fs.readFileSync('telemetry.json', 'utf-8')
+   res.send(data)
+    
+});
+
+app.use(express.urlencoded({
+  extended: true
+}));
